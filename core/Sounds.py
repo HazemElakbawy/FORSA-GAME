7f2595d666cacb344938e848db95ff7faa90bb7c
ahmed_salah <medoghonime@gmail.com>
2023-05-14 16:15:36 +0300

    Sounds partly edited and added winner  & lost screen & buttons

--- core/Sounds.py
+++ core/Sounds.py
@@ -2,6 +2,7 @@ import pygame
 
 # TODO replace directory with yours !!
 
+pygame.mixer.pre_init(44100, -16, 2, 256)
 pygame.init()
 pygame.mixer.init()
 start_sound = pygame.mixer.Sound(
@@ -14,14 +15,17 @@ button_sound = pygame.mixer.Sound(
     "Sounds Assets/button.wav")
 
 movement_sound = pygame.mixer.Sound(
-    "Sounds Assets/Porsche Gt3.mp3")
+    "Sounds Assets/CadillacLowRevIdlL PE862409.wav")
 
 calcson = pygame.mixer.Sound(
     "Sounds Assets/calcson.wav")
 
-# crash_car=pygame.mixer.Sound("Sounds Assets/car_crash_car.wav")
+crash_car =pygame.mixer.Sound("Sounds Assets/car_crash_car.wav")
 
 # crash_wall=pygame.mixer.Sound("Sounds Assets/car_crash_wall.wav")
 
 win = pygame.mixer.Sound(
     "Sounds Assets/win_game.wav")
+
+lost = pygame.mixer.Sound(
+    "Sounds Assets/youlost.wav")
